47ed0c2d51b64d7f11d031932eb86a94019fd456
pjt3591oo <pjt3591oo@gmail.com>
2019-08-18 21:29:41 +0900

    ch8에서 startToken.sh 파일 내용을 채운다

---
+++ ch8/fabric-samples/token/startToken.sh
@@ -0,0 +1,17 @@
+set -e
+
+export MSYS_NO_PATHCONV=1
+starttime=$(date +%s)
+CC_SRC_LANGUAGE=${1:-"go"}
+CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
+CC_RUNTIME_LANGUAGE=golang
+CC_SRC_PATH=github.com/token
+
+rm -rf ./hfc-key-store
+cd ../basic-network
+./start.sh
+
+docker-compose -f ./docker-compose.yml up -d cli
+
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n token -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n token -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":["ParkJeongTae","PJT","10000000"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
